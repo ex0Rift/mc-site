e160df739ed15984f70d0c178fdbce44477536ef
Nate <daviesnathanael45@gmail.com>
2026-02-07 22:35:09 +0000

    added https keys (no they arent in git)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 8080;
-const code_version = "1.0.0";
+const port = 443;
+const code_version = "1.0.1";
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -11,7 +13,12 @@ app.get('/', (rew,res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () =>{
-    console.log(`server running at http://localhost:${port}/`);
+const options = {
+    key: fs.readFileSync('origin.key'),
+    cert: fs.readFileSync('origin.pem')
+};
+
+https.createServer(options, app).listen(port, () => {
+    console.log(`server running at https://localhost:${port}/`);
     console.log(`version of webserver is: ${code_version}`)
 });
