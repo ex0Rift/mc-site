67ec58719cdf1de92f336229eb2692f31e8c7f16
Nate <daviesnathanael45@gmail.com>
2026-02-13 18:55:56 +0000

    added the mods section to the website, editable in the attributes file

--- public/script.js
+++ public/script.js
@@ -3,6 +3,26 @@ status_puc = document.getElementById("indicator");
 version_text = document.getElementById("version-text");
 player_count_text = document.getElementById("player-counter");
 
+download_link = document.getElementById("link");
+loader_text = document.getElementById("loader");
+
+//change the attriutes from attributes.json file which can be edited by server owner
+fetch('/data/attributes.json')
+    .then(r => r.json())
+    .then(attributes => {
+
+        // ** attributes for MOD list **
+        if (!attributes.mods.inUse){
+            document.getElementById("mods_box").style.display = "none";
+        } else {
+            download_link.href = attributes.mods.link;
+            loader_text.textContent = `Loader: ${attributes.mods.modLoader}`
+        }
+
+    });
+
+
+//update live minecraft server data from mcstatus.io
 fetch("https://api.mcstatus.io/v2/status/java/server.natesite.co.uk")
     .then(r => r.json())
     .then(d => {
